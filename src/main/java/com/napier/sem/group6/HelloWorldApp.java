05f205eb3ae5151bac9cb7f25a90fbcd213da624
Clark Bolan <40456381@live.napier.ac.uk>
2020-01-19 17:31:35 +0000

    Test Application

---
+++ src/main/java/com/napier/sem/group6/HelloWorldApp.java
@@ -0,0 +1,9 @@
+package com.napier.sem.group6;
+
+public class HelloWorldApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Group 6's first application");
+    }
+}
